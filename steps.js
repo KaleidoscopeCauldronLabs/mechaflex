ce708ed58b9350e5a22ad820d721c8d21c46e170
messymouse <hypermice@gmail.com>
2025-09-18 21:14:34 -0400

    first commit

--- steps.js
+++ steps.js
@@ -20,17 +20,18 @@ export const steps = [
             <h2>Parts List</h2>
             <p>The Mechaflex kit should contain a paper copy of this parts list. If not they can be found here: <a href="./models/Parts_List_Miles.pdf" target="_blank" rel="noopener noreferrer">Miles</a> | <a href="./models/Parts_List_Frankie.pdf" target="_blank" rel="noopener noreferrer">Frankie</a>. Lay out each piece on your paper copy as you sort.</p>
             <p><i>Note: In order for the pieces to be scaled correctly when printing, on the print interface click "More settings" and select "Actual Size" or "Default" rather than "Fit to Page," "Fit to Printable Area," or "Fit to Paper"</i></p>
+            <p>This build guide will use the parts in approximately the order they are listed on the parts list, so sorting will help everything go more smoothly.</p>
             <p>Use the color pickers on the left to set the colors of the frame and armor to match your kit.</p>
             <p><i>Note: Even if your frame is black, it is not recommend to set the frame color to pure black, as it makes it really hard to see details. A very dark gray (51,51,51) works well.</i></p>
             <p>Be aware, some parts look very similar to others. Feel free to rotate and zoom in to see details of each part.</p>
             <p>The arms and legs sections have some repeated parts, 2 in the 2nd column (looks like Y) and 2 in the 3rd column (looks like sideways w)</p>
-            <p>Add the tools and hands to their holders while sorting.</p>
         ` },
     { title: "Head and Neck", 
       model: "./models/head_neck.glb", 
       tiltX: 350, 
       description: `
             <h2>Head and Neck</h2>
+            <p><i>Note: All steps will be presented this way, with an exploded view with arrows of the current step on the left/front, along with what it looks like when complete on the right/back.</i></p>
             <p>Press the head frame piece into the base of the head armor piece. Snap the neck joint into it, and add the neck armor.</p>
             <p>The head frame piece in this step can be a bit tricky to get all the way in. If needed you can use a small tool or stick to help shove it into place.</p>
         ` },
@@ -38,41 +39,41 @@ export const steps = [
       model: "./models/chest.glb", 
       description: `
             <h2>Chest</h2>
-            <p>Press the lower chest armor onto the chest frame, then the upper chest armor onto the frame. The holes in the back of the armor pieces should line up.</p>
+            <p>Press the lower chest armor onto the chest frame, then the upper chest armor onto the frame. The square holes in the back of the armor pieces and frame should all line up.</p>
         ` },
     { title: "Waist", 
-      model: "./models/.glb", 
+      model: "./models/waist.glb", 
       tiltX: 350, 
       description: `
-            <h2></h2>
+            <h2>Waist</h2>
             <p></p>
         ` },
     { title: "Hips a", 
-      model: "./models/.glb", 
+      model: "./models/hips_a.glb", 
       tiltX: 350, 
       description: `
-            <h2></h2>
+            <h2>Hips a</h2>
             <p></p>
         ` },
     { title: "Hips b", 
-      model: "./models/.glb", 
+      model: "./models/hips_b.glb", 
       tiltX: 350, 
       description: `
-            <h2></h2>
+            <h2>Hips b</h2>
             <p></p>
         ` },
     { title: "Hips c", 
-      model: "./models/.glb", 
+      model: "./models/hips_c.glb", 
       tiltX: 350, 
       description: `
-            <h2></h2>
+            <h2>Hips c</h2>
             <p></p>
         ` },
     { title: "Attach Body", 
-      model: "./models/.glb", 
+      model: "./models/attach_body.glb", 
       tiltX: 350, 
       description: `
-            <h2></h2>
+            <h2>Attach Body</h2>
             <p></p>
         ` },
 
@@ -80,124 +81,138 @@ export const steps = [
       model: "./models/.glb", 
       tiltX: 350, 
       description: `
-            <h2></h2>
+            <h2>Upper Arm a</h2>
             <p></p>
         ` },
     { title: "Upper Arm b", 
       model: "./models/.glb", 
       tiltX: 350, 
       description: `
-            <h2></h2>
+            <h2>Upper Arm b</h2>
             <p></p>
         ` },
     { title: "Lower Arm", 
       model: "./models/.glb", 
       tiltX: 350, 
       description: `
-            <h2></h2>
+            <h2>Lower Arm</h2>
             <p></p>
         ` },
-    { title: "Elbow and Hand", 
+    { title: "Elbow", 
       model: "./models/.glb", 
       tiltX: 350, 
       description: `
-            <h2></h2>
+            <h2>Elbow</h2>
             <p></p>
         ` },
     { title: "Shoulder a", 
       model: "./models/.glb", 
       tiltX: 350, 
       description: `
-            <h2></h2>
+            <h2>Shoulder a</h2>
             <p></p>
         ` },
     { title: "Shoulder b", 
       model: "./models/.glb", 
       tiltX: 350, 
       description: `
-            <h2></h2>
+            <h2>Shoulder b</h2>
+            <p></p>
+        ` },
+    { title: "Hands", 
+      model: "./models/.glb", 
+      tiltX: 350, 
+      description: `
+            <h2>Hands</h2>
             <p></p>
         ` },
     { title: "Attach Arms", 
       model: "./models/.glb", 
       tiltX: 350, 
       description: `
-            <h2></h2>
+            <h2>Attach Arms</h2>
             <p></p>
         ` },
     { title: "Upper Leg a", 
       model: "./models/.glb", 
       tiltX: 350, 
       description: `
-            <h2></h2>
+            <h2>Upper Leg a</h2>
             <p></p>
         ` },
     { title: "Upper Leg b", 
       model: "./models/.glb", 
       tiltX: 350, 
       description: `
-            <h2></h2>
+            <h2>Upper Leg b</h2>
             <p></p>
         ` },
     { title: "Lower Leg", 
       model: "./models/.glb", 
       tiltX: 350, 
       description: `
-            <h2></h2>
+            <h2>Lower Leg</h2>
             <p></p>
         ` },
     { title: "Knee", 
       model: "./models/.glb", 
       tiltX: 350, 
       description: `
-            <h2></h2>
+            <h2>Knee</h2>
             <p></p>
         ` },
     { title: "Thigh", 
       model: "./models/.glb", 
       tiltX: 350, 
       description: `
-            <h2></h2>
+            <h2>Thigh</h2>
             <p></p>
         ` },
     { title: "Feet", 
       model: "./models/.glb", 
       tiltX: 350, 
       description: `
-            <h2></h2>
+            <h2>Feet</h2>
             <p></p>
         ` },
     { title: "Attach Legs", 
       model: "./models/.glb", 
       tiltX: 350, 
       description: `
-            <h2></h2>
+            <h2>Attach Legs</h2>
             <p></p>
         ` },
     { title: "Stand", 
       model: "./models/.glb", 
       tiltX: 350, 
       description: `
-            <h2></h2>
+            <h2>Stand</h2>
+            <p></p>
+        ` },
+    { title: "Stand Adapter", 
+      model: "./models/.glb", 
+      tiltX: 350, 
+      description: `
+            <h2>Stand Adapter</h2>
             <p></p>
         ` },
     { title: "Attach Stand", 
       model: "./models/.glb", 
       tiltX: 350, 
       description: `
-            <h2></h2>
+            <h2>Attach Stand</h2>
             <p></p>
         ` },
     { title: "Tools", 
       model: "./models/.glb", 
       tiltX: 350, 
       description: `
-            <h2></h2>
+            <h2>Tools</h2>
             <p></p>
         ` },
     { title: "Complete!", 
       model: "./models/full.glb", 
-      tiltX: 350, 
+      tiltY: 90, 
       description: `
             <h2>Congratulations!</h2>
             <p>You should now have your completed Mechaflex, enjoy your creation!</p>
